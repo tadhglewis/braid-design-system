e558e0ff8e5b082a73fca8ea8ab4d4710694fb34
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2019-11-28 13:49:01 +1100

    refactor: Update patterns

--- lib/components/Radio/Radio.docs.tsx
+++ lib/components/Radio/Radio.docs.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import dedent from 'dedent';
 import { ComponentDocs } from '../../../site/src/types';
 import { Radio } from './Radio';
 import { Text } from '../Text/Text';
@@ -55,64 +54,6 @@ const docs: ComponentDocs = {
           <Text>This text is visible when the radio button is checked.</Text>
         </Radio>
       ),
-    },
-  ],
-  patterns: [
-    {
-      name: 'Unchecked',
-      code: dedent`
-        <Radio
-          checked={false}
-          id="id"
-          label="Label"
-        />
-      `,
-    },
-    {
-      name: 'Checked',
-      code: dedent`
-        <Radio
-          checked
-          id="id"
-          label="Label"
-        />
-      `,
-    },
-    {
-      name: 'Disabled',
-      code: dedent`
-        <Radio
-          disabled
-          checked={false}
-          id="id"
-          label="Label"
-        />
-      `,
-    },
-    {
-      name: 'Critical',
-      code: dedent`
-        <Radio
-          checked={false}
-          id="id"
-          label="Label"
-          tone="critical"
-        />
-      `,
-    },
-    {
-      name: 'Nested',
-      code: dedent`
-        <Radio
-          checked={true}
-          id="id"
-          label="Label"
-        >
-          <Text>
-            This text is visible when the radio button is checked.
-          </Text>
-        </Radio>
-      `,
     },
   ],
 };
